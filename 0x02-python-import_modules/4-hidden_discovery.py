d403ff1ccdaef4f39424444e433af83932da90ee
HarryCelt <harrycelt2@gmail.com>
2022-07-01 10:50:11 -0700

    corrections

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     arr = dir()
-    for i in range (0, len(arr)):
+    for i in range(0, len(arr)):
         if arr[i][0:2] != "__":
             print("{}".format(arr[i]))
