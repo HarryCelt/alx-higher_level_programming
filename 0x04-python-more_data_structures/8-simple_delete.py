12237b5ae9c86d60cedfa8a15157aeaf4fbc86b9
HarryCelt <harrycelt2@gmail.com>
2022-07-06 18:45:07 -0700

    more data structures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@ def simple_delete(a_dictionary, key=""):
     try:
         del a_dictionary[key]
         return (a_dictionary)
-    except keyError:
+    except KeyError:
         return (a_dictionary)
