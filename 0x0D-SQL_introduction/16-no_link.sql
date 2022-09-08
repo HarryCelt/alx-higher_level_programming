404ad386997eabe193181dbfb7155d356f8acb05
HarryCelt <harrycelt2@gmail.com>
2022-09-07 22:35:54 -0700

    MySQL intro

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
---list all records of the second_table
+-- list all records of the second_table
 -- of the database hbtn_0c_0
 -- rows with no name value are not listed
 -- and records are in descending order
