0879545070111fdd8db6d9d030d141120b920144
HarryCelt <harrycelt2@gmail.com>
2022-09-07 22:21:42 -0700

    MySQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
---list all records of the second_table of the database hbtn_0c_0
--- rows with no name value are not listed and records are in descending order
+--list all records of the second_table
+-- of the database hbtn_0c_0
+-- rows with no name value are not listed
+-- and records are in descending order
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
